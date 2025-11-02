d2e2de31184f1addaf4d38b8a83f665165409c66
Nikson Ferreira <niksonlima135@gmail.com>
2025-11-02 15:10:08 -0300

    Fix porta dinâmica e caminho do banco para Render

--- app/database.py
+++ app/database.py
@@ -13,8 +13,9 @@ DB_NAME = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tmp/moneytrack.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 Base = declarative_base()
 
